22931f51146ec890f00167b047f1b1d88bd08adc
kyj <kyj@micube.co.kr>
2026-02-25 15:31:27 +0900

    Vite 연동 React 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">Tailwind 작동 완료!</h1>
+    </div>
+  )
+}
+export default App
